2a5114ffe99c8766d8e53accc9195ef6f13dc153
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-02-26 02:06:15 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by saeem  on 26/02/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
