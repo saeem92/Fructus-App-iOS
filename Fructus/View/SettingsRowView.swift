f2bc82c7ba7dfd0d312c77edea0998410d2d060a
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-03-04 00:18:25 +0530

    Created All The Views. Added Fruits Data in the App. Finalized App

---
+++ Fructus/View/SettingsRowView.swift
@@ -0,0 +1,55 @@
+//
+//  SettingRowView.swift
+//  Fructus
+//
+//  Created by saeem  on 04/03/22.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+  // MARK: - PROPERTIES
+  
+  var name: String
+  var content: String? = nil
+  var linkLabel: String? = nil
+  var linkDestination: String? = nil
+
+  // MARK: - BODY
+
+  var body: some View {
+    VStack {
+      Divider().padding(.vertical, 4)
+      
+      HStack {
+        Text(name).foregroundColor(Color.gray)
+        Spacer()
+        if (content != nil) {
+          Text(content!)
+        } else if (linkLabel != nil && linkDestination != nil) {
+          Link(linkLabel!, destination: URL(string: "https://\(linkDestination!)")!)
+          Image(systemName: "arrow.up.right.square").foregroundColor(.pink)
+        }
+        else {
+          EmptyView()
+        }
+      }
+    }
+  }
+}
+
+// MARK: - PREVIEW
+
+struct SettingsRowView_Previews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      SettingsRowView(name: "Developer", content: "Saeem")
+        .previewLayout(.fixed(width: 375, height: 60))
+        .padding()
+      SettingsRowView(name: "Website", linkLabel: "Portfolio site", linkDestination: "saeemportfolio.netlify.app")
+        .preferredColorScheme(.dark)
+        .previewLayout(.fixed(width: 375, height: 60))
+        .padding()
+    }
+  }
+}
